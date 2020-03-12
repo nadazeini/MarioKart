070c9578cd1a9c447d5e893873c74e95e818191b
Nada Zeini <nada.elzeini@sjsu.edu>
2020-03-11 22:42:02 -0700

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Nada Zeini on 3/11/20.
+//  Copyright © 2020 Nada Zeini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
