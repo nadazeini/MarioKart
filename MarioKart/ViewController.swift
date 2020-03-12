d9940af765b2002c388719133b08ced850b00acb
Nada Zeini <nada.elzeini@sjsu.edu>
2020-03-11 23:16:57 -0700

    pinch & drag

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+           // Do any additional setup after loading the view.
+       }
+    @IBAction func didPanKartView4(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+        kartView.center = location
+        print("Location: x: \(location.x), y: \(location.y)")
     }
-
-
+    @IBAction func didPanKartView2(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+               kartView.center = location
+        print("Location: x: \(location.x), y: \(location.y)")
+    }
+    @IBAction func didPanKartView3(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+               kartView.center = location
+        print("Location: x: \(location.x), y: \(location.y)")
+    }
+    @IBAction func didPanKartView1(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+               kartView.center = location
+        print("Location: x: \(location.x), y: \(location.y)")
+    }
+    @IBAction func didPanKartView0(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+               kartView.center = location
+        print("Location: x: \(location.x), y: \(location.y)")
+    }
+   
+    @IBAction func didPinchKart0(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    
+    @IBAction func didPinchKart1(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    
+    @IBAction func didPinchKart2(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    @IBAction func didPinchKart3(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    @IBAction func didPinchKart4(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
 }
 
